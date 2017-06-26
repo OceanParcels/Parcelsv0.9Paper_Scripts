090f4eefdc59c2775a4e471e188c2532b9c0dc30
Erik van Sebille <erikvansebille@gmail.com>
2017-06-26 11:20:42 +0200

    bash shell script to download ofes data for agulhas foraminifera experiment

---
+++ experiment_scripts/get_ofesdata_agulhas.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ofesdata
+for i in {3165..3287}
+do
+  export out=`printf "%05d\n" $i`
+  echo "getting files at time $i"
+  ncks -v 'uvel' -d time,$i -d lon,50,351 -d lat,300,501  http://apdrc.soest.hawaii.edu:80/dods/public_ofes/OfES/ncep_0.1_global_3day/uvel -o ofesdata/uvel${out}.nc
+  ncks -v 'vvel' -d time,$i -d lon,50,351 -d lat,300,501  http://apdrc.soest.hawaii.edu:80/dods/public_ofes/OfES/ncep_0.1_global_3day/vvel -o ofesdata/vvel${out}.nc
+  ncks -v 'temp' -d time,$i -d lon,50,351 -d lat,300,501  http://apdrc.soest.hawaii.edu:80/dods/public_ofes/OfES/ncep_0.1_global_3day/temp -o ofesdata/temp${out}.nc
+done
