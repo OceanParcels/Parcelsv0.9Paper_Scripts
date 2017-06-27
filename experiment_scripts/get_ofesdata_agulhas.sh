81bf7324b8ac426fe903c7bea69f1f2656855347
Erik van Sebille <erikvansebille@gmail.com>
2017-06-27 10:50:03 +0200

    adding vertical velocity to download script

--- experiment_scripts/get_ofesdata_agulhas.sh
+++ experiment_scripts/get_ofesdata_agulhas.sh
@@ -6,5 +6,6 @@ do
   echo "getting files at time $i"
   ncks -v 'uvel' -d time,$i -d lon,50,351 -d lat,300,501  http://apdrc.soest.hawaii.edu:80/dods/public_ofes/OfES/ncep_0.1_global_3day/uvel -o ofesdata/uvel${out}.nc
   ncks -v 'vvel' -d time,$i -d lon,50,351 -d lat,300,501  http://apdrc.soest.hawaii.edu:80/dods/public_ofes/OfES/ncep_0.1_global_3day/vvel -o ofesdata/vvel${out}.nc
+  ncks -v 'wvel' -d time,$i -d lon,50,351 -d lat,300,501  http://apdrc.soest.hawaii.edu:80/dods/public_ofes/OfES/ncep_0.1_global_3day/wvel -o ofesdata/wvel${out}.nc
   ncks -v 'temp' -d time,$i -d lon,50,351 -d lat,300,501  http://apdrc.soest.hawaii.edu:80/dods/public_ofes/OfES/ncep_0.1_global_3day/temp -o ofesdata/temp${out}.nc
 done
