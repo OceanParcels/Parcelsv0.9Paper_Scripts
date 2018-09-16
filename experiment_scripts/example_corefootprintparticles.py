b09970afbdefaa6006e350907f2410d804c93d30
Erik van Sebille <erikvansebille@gmail.com>
2018-09-16 10:38:56 +0200

    Updating corefootprintparticles to Parcels v1.1.0

--- experiment_scripts/example_corefootprintparticles.py
+++ experiment_scripts/example_corefootprintparticles.py
@@ -1,7 +1,6 @@
 from parcels import (FieldSet, ParticleSet, JITParticle, AdvectionRK4_3D,
-                     ErrorCode, ParticleFile, Variable, convert_IndexedOutputToArray)
+                     ErrorCode, ParticleFile, Variable)
 from datetime import timedelta as delta
-from progressbar import ProgressBar
 import numpy as np
 from os import path
 import math
@@ -17,9 +16,9 @@ def set_ofes_fieldset(snapshots):
     dimensions = {'lat': 'lat', 'lon': 'lon', 'time': 'time', 'depth': 'lev'}
 
     fieldset = FieldSet.from_netcdf(filenames, variables, dimensions)
-    fieldset.U.data /= 100.  # convert from cm/s to m/s
-    fieldset.V.data /= 100.  # convert from cm/s to m/s
-    fieldset.W.data /= 100.  # convert from cm/s to m/s
+    fieldset.U.set_scaling_factor(0.01)  # convert from cm/s to m/s
+    fieldset.V.set_scaling_factor(0.01)  # convert from cm/s to m/s
+    fieldset.W.set_scaling_factor(0.01)  # convert from cm/s to m/s
     return fieldset
 
 
@@ -46,8 +45,8 @@ def DeleteParticle(particle, fieldset, time, dt):
 
 
 def run_corefootprintparticles(outfile):
-    snapshots = range(3165, 3289)
-    fieldset = set_ofes_fieldset(snapshots[-4:-1])
+    snapshots = range(3165, 3288)
+    fieldset = set_ofes_fieldset(snapshots)
     fieldset.add_constant('dwellingdepth', 50.)
     fieldset.add_constant('sinkspeed', 200./86400)
     fieldset.add_constant('maxage', 30.*86400)
@@ -63,22 +62,17 @@ def run_corefootprintparticles(outfile):
     pset = ParticleSet(fieldset=fieldset, pclass=ForamParticle, lon=corelon, lat=corelat,
                        depth=coredepth, time=fieldset.U.time[-1],
                        repeatdt=delta(days=3))  # the new argument 'repeatdt' means no need to call pset.add() anymore in for-loop
-    pfile = ParticleFile(outfile, pset, type="indexed", outputdt=delta(days=1))  # `interval` argument has changed to `outputdt`
+    pfile = ParticleFile(outfile, pset, outputdt=delta(days=1))  # `interval` argument has changed to `outputdt`
 
     kernels = pset.Kernel(AdvectionRK4_3D) + Sink + SampleTemp + Age
 
-    pbar = ProgressBar()
-    for s in pbar(range(len(snapshots)-5, -1, -1)):
-        pset.execute(kernels, runtime=delta(days=3), dt=delta(minutes=-5), output_file=pfile,
-                     recovery={ErrorCode.ErrorOutOfBounds: DeleteParticle})
-
-        fieldset.advancetime(set_ofes_fieldset([snapshots[s]]))
+    pset.execute(kernels, dt=delta(minutes=-5), output_file=pfile,
+                 recovery={ErrorCode.ErrorOutOfBounds: DeleteParticle})
 
 
 def make_plot(trajfile):
     from netCDF4 import Dataset
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.basemap import Basemap
+    from parcels.plotting import create_parcelsfig_axis
 
     class ParticleData(object):
         def __init__(self):
@@ -93,28 +87,21 @@ def make_plot(trajfile):
         return T
 
     T = load_particles_file(trajfile, ['lon', 'lat', 'temp', 'z'])
-    m = Basemap(projection='merc', llcrnrlat=-40, urcrnrlat=-27.5, llcrnrlon=10, urcrnrlon=32.5, resolution='h')
-    m.drawcoastlines()
-    m.fillcontinents(color='burlywood')
-    m.drawparallels(np.arange(-50, -20, 10), labels=[True, False, False, False])
-    m.drawmeridians(np.arange(0, 40, 10), labels=[False, False, False, True])
+
+    plt, fig, ax, cartopy = create_parcelsfig_axis(spherical=True)
 
     sinks = np.where(T.z > 50.)
     dwell = np.where(T.z == 50.)
-    xs, ys = m(T.lon[dwell], T.lat[dwell])
-    m.scatter(xs, ys, c=T.temp[dwell], s=5)
+    plt.scatter(T.lon[dwell], T.lat[dwell], c=T.temp[dwell], s=5)
     cbar = plt.colorbar()
     cbar.ax.xaxis.set_label_position('top')
     cbar.ax.set_xlabel('[$^\circ$C]')
 
-    xs, ys = m(T.lon[sinks], T.lat[sinks])
-    m.scatter(xs, ys, c='k', s=5)
-    xs, ys = m(T.lon[0, 0], T.lat[0, 0])
-    m.plot(xs, ys, 'om')
+    plt.scatter(T.lon[sinks], T.lat[sinks], c='k', s=5)
+    plt.plot(T.lon[0, 0], T.lat[0, 0], 'om')
     plt.show()
 
 
 outfile = "corefootprint_particles"
 run_corefootprintparticles(outfile)
-convert_IndexedOutputToArray(file_in=outfile+".nc", file_out=outfile+"_array.nc")
-make_plot(outfile+"_array.nc")
+make_plot(outfile+".nc")
